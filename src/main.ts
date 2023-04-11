d6a74268789c0e787f8b3f4d97f5ed15dc71db7e
Yshinu <yshinu2833@gmail.com>
2023-04-11 13:16:36 +0800

    重构main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,18 +1,16 @@
 import { createApp } from 'vue'
 import { App } from './App'
-import { Foo } from './views/Foo'
-import { Bar } from './views/Bar'
-import {createRouter,createWebHashHistory} from 'vue-router'
+
+import {createRouter} from 'vue-router'
+import { routes } from './config/routes'
+import { history } from './shared/history'
 // 2. 定义一些路由
 // 每个路由都需要映射到一个组件。
 // 我们后面再讨论嵌套路由。
-const routes = [
-  { path: '/', component: Foo },
-  { path: '/about', component: Bar },
-]
+
 const router = createRouter({
-    history: createWebHashHistory(),
-    routes, 
+    history,
+    routes:routes, 
   })
   
   const app = createApp(App)
